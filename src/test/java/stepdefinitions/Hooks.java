a7231a8a7d6a99c2aa0d39d48187fc539ec109fc
KlavinsArmands <40773004+KlavinsArmands@users.noreply.github.com>
2018-10-30 09:42:26 +0200

    Added Hooks

---
+++ src/test/java/stepdefinitions/Hooks.java
@@ -0,0 +1,29 @@
+package stepdefinitions;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+import static com.codeborne.selenide.Selenide.close;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.CHROME;
+
+public class Hooks {
+
+    @Before
+    public void launchBrowser(){
+        System.setProperty("selenide.browser", CHROME);
+
+    }
+
+    @Before
+    public void openHomePage() {
+        open("https://www.goeuro.com/");
+    }
+
+
+    @After
+    public void closeBrowser(){
+        close();
+    }
+
+}
